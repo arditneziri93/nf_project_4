abf1f765be8371809207be863523a2b77d550788
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-04-29 20:20:47 +0200

    Use color form in ui
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { initialColors } from "./lib/colors";
+import { ColorType, initialColors } from "./lib/colors";
 import "./App.css";
-import ColorCard from "./Color/ColorCard";
+import ColorCard from "./components/ColorCard/ColorCard";
+import ColorForm from "./components/ColorForm/ColorForm";
 
 function App() {
+  function addColor(color: ColorType) {
+    console.log(color);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-
+      <ColorForm onSubmit={addColor} />
       {initialColors.map((color) => (
         <ColorCard key={color.id} {...color} />
       ))}
