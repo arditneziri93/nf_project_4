a313d444d7e87985edefbc48370f3063bce6e770
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-04-29 20:19:30 +0200

    Create color input
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

---
+++ src/components/ColorForm/ColorInput.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+type ColorInputProps = {
+  initialValue?: string;
+  onChange: (value: string) => void;
+};
+export default function ColorInput(props: ColorInputProps) {
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    setColor(props.initialValue ?? "#000000");
+  }, []);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setColor(event.target.value);
+    props.onChange(event.target.value);
+  }
+
+  return (
+    <>
+      <input value={color} type="text" onChange={handleChange} />
+      <input value={color} type="color" onChange={handleChange} />
+    </>
+  );
+}
