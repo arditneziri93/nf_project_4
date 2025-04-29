aa079f580998e4313632f471c986c288bcf99957
Ardit Neziri <ardit.e.neziri@gmail.com>
2025-04-29 20:57:11 +0200

    Add delete functionality to color card
    Signed-off-by: Ardit Neziri <ardit.e.neziri@gmail.com>

--- src/components/ColorCard/ColorCard.tsx
+++ src/components/ColorCard/ColorCard.tsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import { ColorType } from "../../lib/colors";
 import "./ColorCard.css";
 
-export default function ColorCard({ hex, contrastText, role }: ColorType) {
+interface ColorCardProps extends ColorType {
+  onDelete: (id: string) => void;
+}
+
+export default function ColorCard(props: ColorCardProps) {
+  const [hasRequestedDelete, setHasRequestedDelete] = useState(false);
+
+  function cancelDelete() {
+    setHasRequestedDelete(false);
+  }
+
+  function handleDelete() {
+    if (hasRequestedDelete === false) {
+      setHasRequestedDelete(true);
+    } else {
+      props.onDelete(props.id);
+    }
+  }
+
   return (
     <div
       className="color-card"
       style={{
-        background: hex,
-        color: contrastText,
+        background: props.hex,
+        color: props.contrastText,
       }}
     >
-      <h3 className="color-card-headline">{hex}</h3>
-      <h4>{role}</h4>
-      <p>contrast: {contrastText}</p>
+      <h3 className="color-card-headline">{props.hex}</h3>
+      <h4>{props.role}</h4>
+      <p>contrast: {props.contrastText}</p>
+      {hasRequestedDelete && (
+        <>
+          <span>Really delete?</span>
+          <button onClick={cancelDelete}>Cancel</button>
+        </>
+      )}
+      <button onClick={() => handleDelete()}>Delete</button>
     </div>
   );
 }
